ae65f8f270f056bf113568c6cd6fe750d0018168
Kurt Guardia <kurtguardia@gmail.com>
2020-04-16 21:54:07 +0200

    worked on cache

---
+++ sw.js
@@ -0,0 +1,28 @@
+const staticCacheName = "site-static";
+const assets = [
+  "/",
+  "/index.html",
+  "/js/app.js",
+  "/js/ui.js",
+  "/js/materialize.min.js",
+  "/css/styles.css",
+  "/css/materialize.min.css",
+  "/img/dish.png",
+  "https://fonts.googleapis.com/icon?family=Material+Icons",
+];
+
+self.addEventListener("install", (evt) => {
+  evt.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener("activate", (evt) => {
+  //console.log("activated");
+});
+
+self.addEventListener("fetch", (evt) => {
+  //do something
+});
